0c333e337197973a9ff05aa090ff26a44a25c8c9
PestanaS273 <118370117+PestanaS273@users.noreply.github.com>
2024-07-04 09:51:07 +0200

    add serialization and deserialization, send and broadcast methods

--- src/node/Node.java
+++ src/node/Node.java
@@ -1,6 +1,11 @@
 package node;
 
 import messages.Messages;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -14,6 +19,20 @@ public class Node {
     private ConcurrentHashMap<String, InetAddress> peers;
     private PriorityBlockingQueue<Messages> messageQueue;
 
+    private byte[] serializeMessage(Messages message) throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(message);
+        oos.flush();
+        return bos.toByteArray();
+    }
+
+    private Messages deserializeMessage(byte[] buffer) throws Exception {
+        ByteArrayInputStream bis = new ByteArrayInputStream(buffer);
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        return (Messages) ois.readObject();
+    }
+
     public Node(String nodeName, int port) throws Exception {
         this.nodeName = nodeName;
         this.port = port;
@@ -26,5 +45,38 @@ public class Node {
         peers.put(nodeName, address);
     }
 
+    public void sendMessage(Messages message, String recipientNode) throws Exception {
+        InetAddress address = peers.get(recipientNode);
+        if (address != null) {
+            byte[] buffer = serializeMessage(message);
+        }
+    }
 
+    public void broadcastMessage(Messages message) throws Exception {
+        for (InetAddress address : peers.values()) {
+            byte[] buffer = serializeMessage(message);
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
+            socket.send(packet);
+        }
+    }
+
+    public void receiveMessage() throws Exception {
+        byte[] buffer = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        while (true) {
+            socket.receive(packet);
+            Messages message = deserializeMessage(buffer);
+            handleMessage(message);
+        }
+    }
+
+    private void handleMessage(Messages message) {
+        if (message.isBroadcast()) {
+            messageQueue.add(message);
+            while (!messageQueue.isEmpty()) {
+                Messages msg = messageQueue.poll();
+                System.out.println(msg.getSender() + ": " + msg.getContent());
+            }
+        }
+    }
 }
